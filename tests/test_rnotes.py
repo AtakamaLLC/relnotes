177630992515fbfc6bfd714dc08dfcb249ec9b43
erik aronesty <erik@q32.com>
2022-09-21 15:48:43 -0400

    raising

--- tests/test_rnotes.py
+++ tests/test_rnotes.py
@@ -89,8 +89,8 @@ def test_head_is_earliest(capsys, tmp_run_with_notes):
 
 def test_check_branch(capsys, monkeypatch, tmp_run_with_notes):
     # don't use this for this test
-    monkeypatch.delenv("GITHUB_BASE_REF")
-    monkeypatch.delenv("CI_MERGE_REQUEST_TARGET_BRANCH_NAME")
+    monkeypatch.delenv("GITHUB_BASE_REF", raising=False)
+    monkeypatch.delenv("CI_MERGE_REQUEST_TARGET_BRANCH_NAME", raising=False)
 
     r = tmp_run_with_notes
 
